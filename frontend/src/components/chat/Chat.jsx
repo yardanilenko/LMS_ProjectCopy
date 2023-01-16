6625fec99f3879680059958183d5a79a3b1a83c0
Olity <olity1712@gmail.com>
2023-01-16 18:33:13 +0600

    add contacts component

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Chat() {
     return (
-        <div></div>
+        <div>
+
+        </div>
     );
 }
 
--- frontend/src/components/chat/Contacts.jsx
+++ frontend/src/components/chat/Contacts.jsx
@@ -1,8 +1,58 @@
 import React from 'react';
+import{useState, useEffect} from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import ImageIcon from "@mui/icons-material/Image";
+import ListItemText from "@mui/material/ListItemText";
 
-function Contacts() {
+function Contacts({handleOpenChat}) {
+
+    const [chats, setChats] = useState([]);
+
+    useEffect(() => {
+        fetchChats();
+    }, []);
+
+    const fetchChats = () => {
+        fetch('/api/chats', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(res => res.json())
+            .then(data => {
+                setChats(data.chats)
+            })
+    }
+
+
     return (
-        <div></div>
+        <div>
+            <h3>Contacts</h3>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                {chats && chats.map((chat) => (
+                    <ContactItem chat={chat} handleOpenChat={handleOpenChat}/>
+                ))}
+            </List>
+        </div>
+    );
+}
+
+function ContactItem({chat, handleOpenChat}) {
+    return (
+        <ListItem key={chat.id}
+                  onClick={() => {
+                      handleOpenChat(chat.id, chat.room_name);
+                  }}>
+            <ListItemAvatar>
+                <Avatar>
+                    <ImageIcon />
+                </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={chat.room_name} secondary="Recently" />
+        </ListItem>
     );
 }
 
